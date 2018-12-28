53ccebfc18e6b887e315d44b7cc17e8b42727c87
Tyler Quast <tylerquast@mymail.mines.edu>
2018-12-28 02:10:25 -0700

    Pushing to branch

---
+++ Merchant's Tale/Assets/spawnItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnItem : MonoBehaviour
+{
+  public GameObject Prefab;
+
+  public void clicked(){
+    Vector3 position = new Vector3(Random.Range(-9.0f, 2.0f),Random.Range(-5.0f, 1.0f),0);
+    Instantiate(Prefab, position, Quaternion.identity);
+  }
+}
