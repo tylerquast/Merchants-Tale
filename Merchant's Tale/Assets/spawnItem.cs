dd1f32e9de0b80aa0e2400f792d21b912c233af5
Tyler Quast <tylerquast@mymail.mines.edu>
2018-12-29 00:09:09 -0700

    Random Dungeon

--- Merchant's Tale/Assets/spawnItem.cs
+++ Merchant's Tale/Assets/spawnItem.cs
@@ -5,9 +5,36 @@ using UnityEngine;
 public class spawnItem : MonoBehaviour
 {
   public GameObject Prefab;
+  public GameObject success;
+  public GameObject failure;
   int i = 0;
 
+
   public void clicked(){
+    int rand = Random.Range(1,3);
+    Debug.Log("Random number is " + rand);
+    //Success
+    if(rand == 1){
+      StartCoroutine(successRender(2));
+    }
+    else{
+      StartCoroutine(failureDisplay(2));
+    }
+  }
+
+  IEnumerator failureDisplay(float secToWait){
+    Debug.Log("Start");
+    failure.SetActive(true);
+    yield return new WaitForSeconds(secToWait);
+    Debug.Log("end");
+    failure.SetActive(false);
+  }
+  IEnumerator successRender(float secToWait){
+    Debug.Log("Start");
+    success.SetActive(true);
+    yield return new WaitForSeconds(secToWait);
+    Debug.Log("end");
+    success.SetActive(false);
     Vector3 position = new Vector3(Random.Range(-9.0f, 2.0f),Random.Range(-5.0f, 1.0f),0);
     Prefab.name = i.ToString();
     i++;
