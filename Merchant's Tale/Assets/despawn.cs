e5eb40a8b81831710d28963ca3d0615dad0e42a0
Tyler Quast <tylerquast@mymail.mines.edu>
2018-12-28 21:49:00 -0700

    Switching branches to try and learn how to click

---
+++ Merchant's Tale/Assets/despawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class despawn : MonoBehaviour {
+
+  public GameObject item;
+  void Update () {
+    if (Input.GetMouseButtonDown(0)) {
+      Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+      RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+      if (hit.collider != null) {
+        Debug.Log(item.name);
+        Debug.Log("Deleting");
+        Destroy(item);
+      }
+    }
+  }
+
+}
--- Merchant's Tale/Assets/spawnItem.cs
+++ Merchant's Tale/Assets/spawnItem.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class spawnItem : MonoBehaviour
 {
   public GameObject Prefab;
+  int i = 0;
 
   public void clicked(){
     Vector3 position = new Vector3(Random.Range(-9.0f, 2.0f),Random.Range(-5.0f, 1.0f),0);
+    Prefab.name = i.ToString();
+    i++;
     Instantiate(Prefab, position, Quaternion.identity);
   }
 }
